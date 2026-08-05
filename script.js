62592d93e61cda0b70d7a0a3175187ac0c0856eb
mia <miaarabelatunya.bacarat@g.msuiit.edu.ph>
2026-08-05 16:00:46 +0800

    finalized

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const taskInput = document.getElementById("task");
 const taskList = document.getElementById("taskList");
 const listCard = document.getElementById("list-card");
+const clearBtn = document.getElementById("clearAll");
+const addBtn = document.getElementById("addBtn");
 
 function addTask() {
-       console.log("Button clicked!");
+    //console.log("Button clicked!");
     const text = taskInput.value.trim();
 
     if (text === "") {
@@ -13,7 +15,44 @@ function addTask() {
     listCard.classList.add("show");
 
     const li = document.createElement("li");
-    li.textContent = text;
+
+
+    //checkbox
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    const span = document.createElement("span");
+    span.textContent = text;
+    span.classList.add("task-text");
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = "&times";
+    deleteBtn.classList.add("delete-btn");
+
+    checkbox.addEventListener("change", ()=> {
+        if (checkbox.checked) {
+            span.classList.add("completed");
+        } else {
+            span.classList.remove("completed")
+        }
+    });
+
+    deleteBtn.addEventListener("click", ()=>{
+        li.style.opacity = "0";
+        li.style.transform = "translateX(30px)";
+
+        setTimeout(() => {
+            li.remove();
+
+            if (taskList.children.length === 0) {
+                listCard.classList.remove("show");
+            }
+        }, 250);
+    });
+
+    li.appendChild(checkbox);
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
 
     taskList.appendChild(li);
 
@@ -41,3 +80,4 @@ taskInput.addEventListener("keydown", function(e) {
 });
 
 document.getElementById("addBtn").addEventListener("click", addTask);
+document.getElementById("clearAll").addEventListener("click", clearAll);
